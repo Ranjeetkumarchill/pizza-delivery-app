7cea35d8bf8fa504716208049b328bc254f8030e
Ranjeet Kumar <ranjeetkumarchill@gmail.com>
2021-05-26 18:26:05 +0530

    hdjd

--- pizzaapp/models.py
+++ pizzaapp/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 class pizzaModel(models.Model):
-	name=models.CharField(max_length=40)
-	price=models.CharField(max_length=10)
-    def __str__(self):
+	name = models.CharField(max_length=40)
+	price = models.CharField(max_length=10)
+
+	def __str__(self):
 		return "({self.id}): {self.name}:{self.price}"
 
 
 class CustomerModel(models.Model):
-	userid=models.CharField(max_length=40)
-	phoneno=models.CharField(max_length=20)
+	userid = models.CharField(max_length=40)
+	phoneno = models.CharField(max_length=20)
+
 
 class OrderModel(models.Model):
-	username=models.CharField(max_length=40)
-	phoneno=models.CharField(max_length=20)
-	address=models.CharField(max_length=50)
-	ordereditems=models.CharField(max_length=20)
-	status = models.CharField(max_length = 10)
+	username = models.CharField(max_length=40)
+	phoneno = models.CharField(max_length=20)
+	address = models.CharField(max_length=50)
+	ordereditems = models.CharField(max_length=20)
+	status = models.CharField(max_length=10)
